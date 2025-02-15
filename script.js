2366c0b09496f72d27e8849def60a54b55cb99ef
ala44k <nna33356@gmail.com>
2025-02-15 20:11:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('getServersBtn').addEventListener('click', function() {
-    // ???? ??????? ??????
+    // رابط السيرفر سيسكام
     const serverURL = "https://github.com/ala44k/CCCamSouf/blob/main/cccam.txt";
     
-    // ??? ?????? ?? "????" ???
+    // عرض الرابط كـ "رابط" فقط
     const serverLinkDiv = document.getElementById('serverLink');
     serverLinkDiv.style.display = "block";
-    serverLinkDiv.innerHTML = `???? ?????????: <a href="${serverURL}" target="_blank">???</a>`;
+    serverLinkDiv.innerHTML = `links: <a href="${serverURL}" target="_blank">ici</a>`;
 });
